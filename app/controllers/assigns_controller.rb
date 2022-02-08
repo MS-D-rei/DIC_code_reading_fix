6726abe8228c676625ec000f57dba089b6feb5be
MS_D_rei <46997561+MS-D-rei@users.noreply.github.com>
2022-02-08 18:11:33 +0900

    remove from team by team owner or self, can edit by only owner

--- app/controllers/assigns_controller.rb
+++ app/controllers/assigns_controller.rb
@@ -31,7 +31,8 @@ class AssignsController < ApplicationController
       I18n.t('views.messages.cannot_delete_the_leader')
     elsif Assign.where(user_id: assigned_user.id).count == 1
       I18n.t('views.messages.cannot_delete_only_a_member')
-    elsif assign.destroy
+    elsif assigned_user == current_user
+      assign.destroy
       set_next_team(assign, assigned_user)
       I18n.t('views.messages.delete_member')
     else
@@ -62,7 +63,11 @@ class AssignsController < ApplicationController
     change_keep_team(assigned_user, another_team) if assigned_user.keep_team_id == assign.team_id
   end
 
-  def find_team(team_id)
+  def find_team(_team_id)
     Team.friendly.find(params[:team_id])
+  end
+
+  def team_owner_or_current_user
+
   end
 end
